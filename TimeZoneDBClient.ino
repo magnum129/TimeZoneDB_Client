197c529c3d37ce9ba47aa8739115340a3d12bb68
Matthew Gordon <magordon@hacc.edu>
2019-03-15 14:52:09 -0400

    initial

---
+++ TimeZoneDBClient.ino
@@ -0,0 +1,55 @@
+/*
+    This sketch sends data via HTTP GET requests to data.sparkfun.com service.
+
+    You need to get streamId and privateKey at data.sparkfun.com and paste them
+    below. Or just customize this script to talk to other HTTP servers.
+
+*/
+
+#include <ESP8266WiFi.h>
+#include "TimeDB.h"
+
+const char* ssid     = "your-ssid";
+const char* password = "your-password";
+String TIMEDBKEY = "your-timedb-key";
+String LAT = "your-lat";
+String LON = "your-lon";
+
+TimeDB TimeDB("");
+
+void setup() {
+  Serial.begin(115200);
+  delay(10);
+
+  // We start by connecting to a WiFi network
+
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  /* Explicitly set the ESP8266 to be a WiFi-client, otherwise, it by default,
+     would try to act as both a client and an access-point and could cause
+     network-issues with your other WiFi-devices on your WiFi-network. */
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+  TimeDB.updateConfig(TIMEDBKEY, LAT, LON);
+}
+
+int value = 0;
+
+void loop() {
+  delay(5000);
+  Serial.println(now());
+}
+
